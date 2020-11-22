88c65edbaee207427046797bcff08f759c56ac40
Xuan Yang <yangxuanemail@gmail.com>
2020-11-22 15:09:21 -0800

    Small clean up

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,3 @@
-console.log('Client side js file is loaded!');
-
-// fetch('http://localhost:3000/weather?address=boston').then((response) => {
-//     response.json().then((data) => {
-//         if (data.error) {
-//             console.log(data.error);
-//         } else {
-//             console.log('forecast:', data.forecast);
-//             console.log('location:', data.location);
-//         }
-//     });
-// });
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
